1a80a488ed338d7dd34db03c60ffb623626dfda9
Rafael Neiva <rafaelneiva@Internals-MacBook-Pro.local>
2019-07-29 14:55:02 -0300

    remove

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -3,21 +3,6 @@ const firebaseAdmin = require('firebase-admin');
 const {
     Storage
 } = require('@google-cloud/storage');
-// require('@google-cloud/debug-agent').start({
-//     allowExpressions: true
-// });
-
-if (!firebaseAdmin.apps.length) {
-    firebaseAdmin.initializeApp({
-        "projectId": "muzei-mtg",
-        "databaseURL": "https://muzei-mtg.firebaseio.com",
-        "storageBucket": "muzei-mtg.appspot.com",
-        "locationId": "us-central",
-        "apiKey": "AIzaSyBGE5o0ORgSAFdmYYC3Xr3DWuearR46wcA",
-        "authDomain": "muzei-mtg.firebaseapp.com",
-        "messagingSenderId": "551823486447"
-    });
-}
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
